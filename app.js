bd2d7c9f99d05e050ed72f3f4931fb5be67501fc
SelfTitled <davidplarsen@gmail.com>
2020-01-26 09:16:14 -0600

    Implement add item and remove item functionality

--- app.js
+++ app.js
@@ -69,15 +69,24 @@ app.get("/", function(req, res) {
 
 app.post("/", function(req, res) {
 
-  const item = req.body.newItem;
+  const itemName = req.body.newItem;
+  const listEntry = new Item({
+      name: itemName
+  });
 
-  if (req.body.list === "Work") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
-    items.push(item);
-    res.redirect("/");
-  }
+  listEntry.save();
+
+  res.redirect("/");
+});
+
+app.post("/delete", function(req, res){
+  const checkedItemId = req.body.checkbox;
+  Item.findByIdAndRemove(checkedItemId, function(err){
+    if (!err) {
+      console.log("Successfully removed checked item");
+      res.redirect("/");
+    };
+  });
 });
 
 app.get("/work", function(req, res) {
